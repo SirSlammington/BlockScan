0b1676f68cf9ebd9c256d23d2560fb7d46279417
SirSlammington <44537892+SirSlammington@users.noreply.github.com>
2022-04-29 12:52:45 -0700

    Functionality implementations.
    Fixed several methods and changed the logic in the main code block.

--- scan.py
+++ scan.py
@@ -2,40 +2,52 @@
 import subprocess
 from shlex import split
 import nmap
+from colorama import Fore
 
 class Scan:
-    def __init__(self, configuration):
-        self.conf = configuration
+    def __init__(self, host, flags, ports):
+        self.host = host
+        self.flags = flags
+        self.ports = ports
 
     # Method to detect if nmap exists on the system (GNU/Linux only)
-    def checkInstall():
+    def checkInstall(self):
         try:
             subprocess.check_output(split('which nmap'))
             return True
         except subprocess.CalledProcessError as subpError:
-            print(f'\033[1;31;40m SUBPROCESS Error Code: {subpError.returncode}')
+            print(Fore.RED + f' SUBPROCESS Error Code: {subpError.returncode}')
             return False
     
     # Function to iterate through each host
-    ''' Problem for future:
-    Redirecting stdout should be reserved for a separate function outside of this one.'''
-    def scan(hosts, flags, portNums):
-        for host in hosts:
-            # Removes newline character from end of each item in the list
-            host = host.replace('\n', '')
+    ''' 
+    Redirecting stdout should be reserved for a separate function outside of this one.
+    '''
+    def scanTarget(self):
+        host = host.replace('\n', '')
             
-            # Output file
-            out_file = open(f'{host}.txt', 'w')
+        # Output file
+        out_file = open(f'{host}.txt', 'w')
 
-            # Scanner object
-            scanner = nmap.PortScanner()
-            """f'nmap {" ".join(flags)} {host}'"""
-            scan = scanner.scan(host, arguments=flags, ports=portNums)
-            cmd = scanner.command_line()
-            print(cmd)
-            ind_scan = subprocess.Popen(split(cmd), stdout=out_file)
-            ind_scan.wait() # Waits for scan to complete before moving on to subsequent host
+        # Scanner object
+        scanner = nmap.PortScanner()
+        """f'nmap {" ".join(flags)} {host}'"""
+            
+        cmd = scanner.command_line()
+        print(cmd)
+
+        # Performs scan (this updates the scanner object with callable data found in a scan)
+        scanner.scan(host, arguments=self.flags, ports=self.ports)
                 
-            # Writes stdout to a file
-            with out_file:
-                out_file.write(str(ind_scan.stdout))
+        # Writes stdout to a file
+        with out_file:
+            out_file.write(f'HOST: {host} ({scanner[host].hostname()}) \n STATE: {scanner[host].state()}\n')
+
+            for proto in scanner[host].all_protocols():
+                out_file.write(f'PROTO: {proto}')
+                    
+                # Ports in overlying protocol
+                fport = scanner[host][proto].keys()
+                fport.sort()
+                for port in fport:
+                    out_file.write(f'PORT: {port}\tSTATUS: {scanner[host][proto][port]["state"]}')
