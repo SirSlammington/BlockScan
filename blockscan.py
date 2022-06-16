93ba787717ec90755680ed76bdc6fd8555759e83
SirSlammington <44537892+SirSlammington@users.noreply.github.com>
2022-06-15 22:29:09 -0700

    Updates to threading
    Solely for testing purposes. Not intended for use as of yet. Removed a lot of the hard coding.

--- blockscan.py
+++ blockscan.py
@@ -2,10 +2,11 @@
 from scanconfig import ScanConfig
 from scan import Scan
 from colorama import Fore
-from threading import Thread
+import multiprocessing as multp
 
 # Main block
 if __name__ == '__main__':
+    multp.set_start_method("spawn")
     conf = ScanConfig('conf.yaml')
     scan = Scan()
 
@@ -22,27 +23,28 @@ if __name__ == '__main__':
     with open('hosts.txt', 'r') as file:
         agg_hosts = file.readlines()
         while len(agg_hosts) != 0:
+            pool = multp.Pool
             try:
-                scan_one = Thread(target=scan.scanTarget(agg_hosts[0], conf.parseListArgs(conf.getConf('args')), conf.getConf('ports')))
-                scan_two = Thread(target=scan.scanTarget(agg_hosts[1], conf.parseListArgs(conf.getConf('args')), conf.getConf('ports')))
-                scan_three = Thread(target=scan.scanTarget(agg_hosts[2], conf.parseListArgs(conf.getConf('args')), conf.getConf('ports')))
+                '''The "threads" signifier is literally only for writing purposes; there are no threads being executed beyond the first few for the parent processes'''
+                # This loop will create the processes ("threads") and subsequently executes them
+                for threads in range(int(conf.getConf('threads'))):
+                    # Empty list of all the processes to be executed
+                    processes = []
+                    scan_attempt = pool(target=scan.scanTarget, args=(agg_hosts[0], conf.parseListArgs(conf.getConf('args')), conf.getConf('ports')))
+                    processes.append(scan_attempt)
+                    # Starts the "thread"
+                    scan_attempt.start()
+                
+                # Joins the processes
+                for process in processes:
+                    process.join()
 
-                # Starts each scan execution
-                scan_one.start()
-                scan_two.start()
-                scan_three.start()
-
-                # Waits until the main thread is finished before moving on
-                scan_one.join()
-                scan_two.join()
-                scan_three.join()
-
-                # Remove hosts scanned from list
-                for i in range(3):
+                # Deletes the hosts scanned in the list of hosts collected from the hosts file
+                for i in range(int(conf.getConf('threads'))):
                     agg_hosts.pop(i)
             except IndexError:
                 try:
-                    Thread(target=scan.scanTarget(agg_hosts[0], conf.parseListArgs(conf.getConf('args')), conf.getConf('ports'))).start()
+                    multp.Pool(target=scan.scanTarget(agg_hosts[0], conf.parseListArgs(conf.getConf('args')), conf.getConf('ports'))).start()
                     agg_hosts.pop(0)
                 except IndexError:
                     print('No more hosts left to scan.')
